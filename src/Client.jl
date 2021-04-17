a39caf1341fb449302789616bbb2366ce39d76c2
indra <ispandey81@gmail.com>
2021-04-17 19:37:54 +0800

    Changed the server url to a heroku app

--- src/Client.jl
+++ src/Client.jl
@@ -3,7 +3,7 @@ module Client
 using HTTP, JSON3
 using ..Model
 
-const SERVER = Ref{String}("http://localhost:8080")
+const SERVER = Ref{String}("https://friends-first-draft.herokuapp.com")
 
 function getImageById(id)
     resp = HTTP.get(string(SERVER[], "/api/image/id/$id"))
